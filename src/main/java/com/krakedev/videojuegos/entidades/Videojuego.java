9ba58ecddaacfe091389b43fe82c202c05b36a1b
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 18:18:01 -0500

    se crea constructores, uno vacio y otro que recibe parametros

--- src/main/java/com/krakedev/videojuegos/entidades/Videojuego.java
+++ src/main/java/com/krakedev/videojuegos/entidades/Videojuego.java
@@ -7,5 +7,23 @@ public class Videojuego {
 	private double precio;
 	private boolean disponible;
 	private String genero;
+	
+	//constructor vacio
+	public Videojuego() {
+		
+	}
+	//constructor que recibe parametros
+	public Videojuego(String codigo, String nombre, String plataforma, double precio, boolean disponible,
+			String genero) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.plataforma = plataforma;
+		this.precio = precio;
+		this.disponible = disponible;
+		this.genero = genero;
+	}
+	
+	
+	
 
 }
