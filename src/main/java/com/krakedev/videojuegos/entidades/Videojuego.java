a3d7383500f4708fde0f01e21a714eeb6dddb285
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 18:16:18 -0500

    se crea la clase Videojuego con sus atributos privados

---
+++ src/main/java/com/krakedev/videojuegos/entidades/Videojuego.java
@@ -0,0 +1,11 @@
+package com.krakedev.videojuegos.entidades;
+
+public class Videojuego {
+	private String codigo;
+	private String nombre;
+	private String plataforma;
+	private double precio;
+	private boolean disponible;
+	private String genero;
+
+}
