199d799c4e58ce48dd4bb9ca95244ac985a21e63
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 18:33:52 -0500

    se realiza la conexion a Postgres

---
+++ src/main/java/com/krakedev/jdbc/ConexionPostgres.java
@@ -0,0 +1,38 @@
+package com.krakedev.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConexionPostgres {
+
+	private static final Logger log = LoggerFactory.getLogger(ConexionPostgres.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection con = null;
+
+		try {
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tallerjdbc", "postgres", "1004034391");
+			log.info("¡¡¡Conexion exitosa!!!");
+
+		} catch (SQLException e) {
+
+			log.error("¡¡¡Error de conexion!!! " + e.getMessage());
+
+			// cerramos la conexion
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
