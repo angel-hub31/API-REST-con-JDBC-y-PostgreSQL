f49bbbbd166dbbb0d0fce1b7f83fd2623ee7a6d3
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 21:32:32 -0500

    bchd

--- videojuego.sql
+++
@@ -1,9 +0,0 @@
-create table videojuegos(
-codigo varchar(10) primary key,
-nombre varchar(100) not null,
-plataforma varchar (50) not null,
-precio double precision not null,
-disponible boolean not null,
-genero varchar (50)
-
-);
