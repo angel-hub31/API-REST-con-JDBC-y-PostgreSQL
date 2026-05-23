680a6648ee3f3e53941637def4fd8993cd413a7f
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 20:30:48 -0500

    se crea el metodo actualizar

--- src/main/java/com/krakedev/jdbc/videojuegos/services/ServicioVideojuegoJdbc.java
+++ src/main/java/com/krakedev/jdbc/videojuegos/services/ServicioVideojuegoJdbc.java
@@ -9,22 +9,28 @@ import com.krakedev.videojuegos.entidades.Videojuego;
 
 @Service
 public class ServicioVideojuegoJdbc {
-	
+
 	public Videojuego crear(Videojuego videojuego) {
-	return	VideojuegoJdbc.insertar(videojuego.getCodigo(), videojuego.getNombre(),videojuego.getPlataforma(),videojuego.getPrecio(), videojuego.isDisponible(), videojuego.getGenero());
-		
+		return VideojuegoJdbc.insertar(videojuego.getCodigo(), videojuego.getNombre(), videojuego.getPlataforma(),
+				videojuego.getPrecio(), videojuego.isDisponible(), videojuego.getGenero());
+
 	}
-	
-	public List<Videojuego>listar(){
+
+	public List<Videojuego> listar() {
 		return VideojuegoJdbc.listar();
-		
+
 	}
+
 	public Videojuego buscarPorCedula(String codigo) {
 		return VideojuegoJdbc.buscar(codigo);
-		
+
 	}
-	
-	
-	
+
+	public Videojuego actualizar(String codigo, Videojuego videojuegoActualizado) {
+		return VideojuegoJdbc.actualizar(codigo, videojuegoActualizado.getNombre(),
+				videojuegoActualizado.getPlataforma(), videojuegoActualizado.getPrecio(),
+				videojuegoActualizado.isDisponible(), videojuegoActualizado.getGenero());
+
+	}
 
 }
