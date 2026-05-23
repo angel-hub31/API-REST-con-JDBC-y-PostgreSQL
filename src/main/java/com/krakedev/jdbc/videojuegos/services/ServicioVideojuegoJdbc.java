7a11a00c65fdf4972e9292f9987839059293cee4
angel-hub31 <moralesmales1@hotmail.com>
2026-05-22 20:24:07 -0500

    se crea el metodo ArrayList listar

--- src/main/java/com/krakedev/jdbc/videojuegos/services/ServicioVideojuegoJdbc.java
+++ src/main/java/com/krakedev/jdbc/videojuegos/services/ServicioVideojuegoJdbc.java
@@ -1,5 +1,26 @@
 package com.krakedev.jdbc.videojuegos.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.krakedev.jdbc.videojuego.VideojuegoJdbc;
+import com.krakedev.videojuegos.entidades.Videojuego;
+
+@Service
 public class ServicioVideojuegoJdbc {
+	
+	public Videojuego crear(Videojuego videojuego) {
+	return	VideojuegoJdbc.insertar(videojuego.getCodigo(), videojuego.getNombre(),videojuego.getPlataforma(),videojuego.getPrecio(), videojuego.isDisponible(), videojuego.getGenero());
+		
+	}
+	
+	public List<Videojuego>listar(){
+		return VideojuegoJdbc.listar();
+		
+	}
+	
+	
+	
 
 }
